bd6b732f7bc8c4acfe8cb89e0dd263ea82cb55a5
Paul Schaefer <yeppaul2@gmail.com>
2017-01-26 20:36:12 -0500

    updated google keep script, google changed the ID

--- google-keep/script.js
+++ google-keep/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Keep Sidebar Closer
 // @namespace    https://github.com/pschfr/stylish/tree/master/google-keep
-// @version      1.0
+// @version      1.0.1
 // @description  Closes the sidebar on page load
 // @author       Paul Schaefer
 // @match        keep.google.com/*
@@ -9,8 +9,7 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
 	window.onload = function() {
-		document.getElementsByClassName('gb_0b')[0].click();
+		document.getElementsByClassName('gb_1b')[0].click();
 	};
 })();
