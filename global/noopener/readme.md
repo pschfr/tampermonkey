c35caf1a40e6332e498cac96ca286f1ae1e1f9f9
Paul Schaefer <yeppaul2@gmail.com>
2017-02-01 13:17:29 -0500

    added global noopener script

---
+++ global/noopener/readme.md
@@ -0,0 +1,2 @@
+# noopener
+This script appends `rel="noopener noreferrer"` to every link that has `target="_blank"`, as it prevents a major security risk, as detailed [here](https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/).
---
+++ global/noopener/script.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         noopener
+// @namespace    https://github.com/pschfr/tampermonkey/tree/master/global/noopener
+// @version      1.0
+// @description  This script appends `rel="noopener noreferrer"` to every link that has `target="_blank"`, as it prevents a major security risk.
+// @author       Paul Schaefer
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	window.onload = function() {
+		var allLinks = document.getElementsByTagName('a');
+
+		for (var i in allLinks) {
+			if (allLinks[i].target == '_blank') {
+				allLinks[i].rel = 'noopener noreferrer';
+			}
+		}
+	};
+})();
